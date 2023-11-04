f2801f33052483e0bbfd4cc7643df82ac4b4d5f2
MarinaYur <marina.yurkev@gmail.com>
2023-11-04 19:34:17 +0300

    added solved tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -625,8 +625,11 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  const minValue = Math.ceil(min);
+  const maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+  // throw new Error('Not implemented');
 }
 
 /**
